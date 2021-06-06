504d06d4c50a55ec4075dbf5ee7e188894831dea
Brunno Oliveira <brunnogoncalves@gmail.com>
2021-06-06 18:03:13 -0300

    fix: change person mapper to abstract class

--- src/main/java/br/com/bgdo/personapi/mapper/PersonMapper.java
+++ src/main/java/br/com/bgdo/personapi/mapper/PersonMapper.java
@@ -8,12 +8,11 @@ import br.com.bgdo.personapi.dto.request.PersonDTO;
 import br.com.bgdo.personapi.entity.Person;
 
 @Mapper(componentModel = "spring")
-public interface PersonMapper {
-
+public abstract class PersonMapper {
   public static final PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
   @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
-  Person toModel(PersonDTO dto);
+  public abstract Person toModel(PersonDTO dto);
 
-  PersonDTO toDTO(Person dto);
+  public abstract PersonDTO toDTO(Person dto);
 }
